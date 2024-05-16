5fe52aeee0755f07d9db494c595a2c9162d7d789
Clément Nerma <clement.nerma@gmail.com>
2024-05-16 10:14:27 +0200

    Remove TODO comment

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,6 @@ fn main() {
             }
 
             let result = if checked {
-                // TODO: update index IF changed
                 index.query_checked(&query, after.as_deref())
             } else {
                 index
